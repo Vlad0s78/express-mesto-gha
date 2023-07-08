3e3f39d272e70d538914d74313ed99b1c8f5f191
Vlad Cheshev <vlad.toloka37@yandex.ru>
2023-07-09 02:52:45 +0300

    hotfix: HOTfix 1 iterration

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers, getUserById, createUser, updateProfileUser,
+  updateAvatarUser,
+} = require('../controllers/users');
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
+router.patch('/me', updateProfileUser);
+router.patch('/me/avatar', updateAvatarUser);
 
 module.exports = router;
